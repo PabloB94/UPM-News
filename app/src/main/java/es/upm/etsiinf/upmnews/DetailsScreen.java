ec60d9d008cf088a0c335a2e5b06c2821824eb71
Dudekruker <Futbolsala123456>
2020-05-10 20:25:40 +0200

    Adaptacion inicial

--- app/src/main/java/es/upm/etsiinf/upmnews/DetailsScreen.java
+++ app/src/main/java/es/upm/etsiinf/upmnews/DetailsScreen.java
@@ -9,9 +9,9 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import es.upm.etsiinf.upmnews.model.Article;
 import es.upm.etsiinf.upmnews.utils.SerializationUtils;
+import es.upm.etsiinf.upmnews.utils.network.ModelManager;
 
 public class DetailsScreen extends AppCompatActivity {
-    private Article current;
     private String id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +19,6 @@ public class DetailsScreen extends AppCompatActivity {
         setContentView(R.layout.details_screen);
         Intent in = getIntent();
         id = in.getStringExtra("id");
-        //current =(Article) in.getSerializableExtra("Article");//pass the article in intent extras, check this out
     }
 
     @Override
@@ -27,11 +26,12 @@ public class DetailsScreen extends AppCompatActivity {
         super.onResume();
         String textazo="Las respuestas que te dieron son válidas. Lo que si me gustaría agregar es que quizás debas analizar utilizar fragmentos, son menos costosos y te dan una UI fluida. Por otra parte es a lo que Android apunta a con jetpack hoy, 1 actividad simple que solo inicia un navigator y después las vistas en fragmentos. Los pases entre vistas se hacen mucho más fluidos (cada actividad tiene un set de recursos asociados que deben cargarse al iniciar, el fragmento trabaja sobre una actividad)Las respuestas que te dieron son válidas. Lo que si me gustaría agregar es que quizás debas analizar utilizar fragmentos, son menos costosos y te dan una UI fluida. Por otra parte es a lo que Android apunta a con jetpack hoy, 1 actividad simple que solo inicia un navigator y después las vistas en fragmentos. Los pases entre vistas se hacen mucho más fluidos (cada actividad tiene un set de recursos asociados que deben cargarse al iniciar, el fragmento trabaja sobre una actividad)Las respuestas que te dieron son válidas. Lo que si me gustaría agregar es que quizás debas analizar utilizar fragmentos, son menos costosos y te dan una UI fluida. Por otra parte es a lo que Android apunta a con jetpack hoy, 1 actividad simple que solo inicia un navigator y después las vistas en fragmentos. Los pases entre vistas se hacen mucho más fluidos (cada actividad tiene un set de recursos asociados que deben cargarse al iniciar, el fragmento trabaja sobre una actividad)";
         Article ejemplo = new Article("Technology","Articulo de prueba del metodo load","Esto es el resumen del articulo que es muy interesante",textazo,"Subtitulo de la noticia de prueba bro","01");
-        loadElements(ejemplo);
+        loadElements(id);
     }
 
-    private void loadElements(Article load){
+    private void loadElements(String id){
         try{
+            Article load = ModelManager.getArticle(Integer.parseInt(id));
             //load article elements on view
             TextView cat = findViewById(R.id.categoryShow);
             TextView title= findViewById(R.id.titleShow);
