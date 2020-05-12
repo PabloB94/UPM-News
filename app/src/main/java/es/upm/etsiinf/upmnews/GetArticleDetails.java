e9d344337f8f43ee37e0cfd3459f725eb27b9e37
Pablo Beltrán de Casso <beltrancasso@hotmail.com>
2020-05-12 20:25:24 +0200

    Bug fixes on article details

--- app/src/main/java/es/upm/etsiinf/upmnews/GetArticleDetails.java
+++ app/src/main/java/es/upm/etsiinf/upmnews/GetArticleDetails.java
@@ -5,50 +5,24 @@ import android.util.Log;
 
 import es.upm.etsiinf.upmnews.model.Article;
 import es.upm.etsiinf.upmnews.utils.network.ModelManager;
-import es.upm.etsiinf.upmnews.utils.network.exceptions.AuthenticationError;
 import es.upm.etsiinf.upmnews.utils.network.exceptions.ServerCommunicationError;
 
 import static es.upm.etsiinf.upmnews.utils.Logger.TAG;
 
 public class GetArticleDetails extends AsyncTask<Void, Void, Article> {
 
-    //hardcoded stored credentials
-    String strIdUser="";
-    String strApiKey="";
-    String strIdAuthUser="";
-
-    public AsyncResponse delegate = null;
+    AsyncResponse delegate = null;
     private DetailsScreen context;
     private String id;
 
-    public GetArticleDetails(DetailsScreen cont,String id_art){
-        this.context=cont;
-        this.id=id_art;
+    GetArticleDetails(DetailsScreen cont, String id_art){
+        this.context = cont;
+        this.id = id_art;
     }
 
     @Override
     protected Article doInBackground(Void... voids) {
         Article res = null;
-        //ModelManager uses singleton pattern, connecting once per app execution in enough
-        if (!ModelManager.isConnected()){
-            // if it is the first login
-            if (strIdUser==null || strIdUser.equals("")) {
-                try {
-                    ModelManager.login("DEV_TEAM_02", "01394");
-                    strApiKey = ModelManager.getLoggedApiKey();
-                    strIdAuthUser = ModelManager.getLoggedAuthType();
-                    strIdUser = ModelManager.getLoggedIdUSer();
-                } catch (AuthenticationError e) {
-                    Log.e(TAG, e.getMessage());
-                }
-            }
-            // if we have saved user credentials from previous connections
-            else{
-                ModelManager.stayloggedin(strIdUser,strApiKey,strIdAuthUser);
-            }
-        }
-        //If connection has been successful
-        if (ModelManager.isConnected()) {
             try {
                 // obtain article by id
                 res =  ModelManager.getArticle(Integer.parseInt(id));
@@ -56,10 +30,10 @@ public class GetArticleDetails extends AsyncTask<Void, Void, Article> {
             } catch (ServerCommunicationError e) {
                 Log.e(TAG,e.getMessage());
             }
-        }
         return res;
     }
 
     @Override
-    public void onPostExecute(Article res){context.loadElements(res);}
+    public void onPostExecute(Article res){
+        context.loadElements(res);}
 }
